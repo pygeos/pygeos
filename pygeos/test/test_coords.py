5fcf86bcb3302a2f770b6f2155e5acc89745abdc
Casper van der Wel <caspervdw@gmail.com>
2019-09-23 19:34:36 +0200

    Better tests

--- pygeos/test/test_coords.py
+++ pygeos/test/test_coords.py
@@ -1,46 +1,70 @@
 import pytest
 import pygeos
+from pygeos.ufuncs import count_coordinates, get_coordinates
 import numpy as np
 from numpy.testing import assert_equal
 
-from .common import all_types, empty
+from .common import empty
+from .common import point
+from .common import point_z
+from .common import line_string
+from .common import linear_ring
+from .common import polygon
+from .common import polygon_with_hole
+from .common import multi_point
+from .common import multi_line_string
+from .common import multi_polygon
+from .common import geometry_collection
+
+nested_2 = pygeos.geometrycollections([geometry_collection, point])
+nested_3 = pygeos.geometrycollections([nested_2, point])
 
 
-@pytest.fixture
-def geometries():
-    n = 3  # number of geometries per type
-    m_linestring = 5  # coords in linestring
-    n_coords = 3 + 3 * 5
-    coords = np.random.random((n_coords, 2))
-    points = pygeos.points(coords[:3])
-    linestrings = pygeos.linestrings(coords[3:].reshape(n, m_linestring, 2))
-    return np.concatenate([points, linestrings]), coords.T
+@pytest.mark.parametrize(
+    "geoms,count",
+    [
+        ([], 0),
+        ([empty], 0),
+        ([point, empty], 1),
+        ([empty, point, empty], 1),
+        ([point, point], 2),
+        ([point, point_z], 2),
+        ([line_string, linear_ring], 8),
+        ([polygon], 5),
+        ([polygon_with_hole], 10),
+        ([multi_point, multi_line_string], 4),
+        ([multi_polygon], 10),
+        ([geometry_collection], 3),
+        ([nested_2], 4),
+        ([nested_3], 5),
+    ],
+)
+def test_count_coords(geoms, count):
+    actual = count_coordinates(np.array(geoms, np.object))
+    assert actual == count
 
 
-def test_count_coords_empty(geometries):
-    assert pygeos.ufuncs.count_coordinates(np.array([empty, empty])) == 0
-
-
-def test_get_coords_empty(geometries):
-    actual = pygeos.ufuncs.get_coordinates(np.array([empty, empty]))
-    assert actual.shape == (2, 0)
-
-
-def test_count_coords_simple(geometries):
-    assert pygeos.ufuncs.count_coordinates(geometries[0]) == 3 + 3 * 5
-
-
-def test_get_coords_simple(geometries):
-    actual = pygeos.ufuncs.get_coordinates(geometries[0])
-    assert_equal(actual, geometries[1])
-
-
-def test_count_coords_all_types():
-    expected = 31
-    assert pygeos.ufuncs.count_coordinates(np.array(all_types)) == expected
-
-
-def test_get_coords_all_types():
-    expected = (2, 31)
-    actual = pygeos.ufuncs.get_coordinates(np.array(all_types))
-    assert actual.shape == expected
+# fmt: off
+@pytest.mark.parametrize(
+    "geoms,x,y",
+    [
+        ([], [], []),
+        ([empty], [], []),
+        ([point, empty], [2], [3]),
+        ([empty, point, empty], [2], [3]),
+        ([point, point], [2, 2], [3, 3]),
+        ([point, point_z], [2, 1], [3, 1]),
+        ([line_string, linear_ring], [0, 1, 1, 0, 1, 1, 0, 0], [0, 0, 1, 0, 0, 1, 1, 0]),
+        ([polygon], [0, 2, 2, 0, 0], [0, 0, 2, 2, 0]),
+        ([polygon_with_hole], [0, 0, 10, 10, 0, 2, 2, 4, 4, 2], [0, 10, 10, 0, 0, 2, 4, 4, 2, 2]),
+        ([multi_point, multi_line_string], [0, 1, 0, 1], [0, 2, 0, 2]),
+        ([multi_polygon], [0, 1, 1, 0, 0, 2.1, 2.2, 2.2, 2.1, 2.1], [0, 0, 1, 1, 0, 2.1, 2.1, 2.2, 2.2, 2.1]),
+        ([geometry_collection], [51, 52, 49], [-1, -1, 2]),
+        ([nested_2], [51, 52, 49, 2], [-1, -1, 2, 3]),
+        ([nested_3], [51, 52, 49, 2, 2], [-1, -1, 2, 3, 3]),
+    ],
+)  # fmt: on
+def test_get_coords(geoms, x, y):
+    actual = get_coordinates(np.array(geoms, np.object))
+    expected = np.array([x, y], np.float64)
+    assert_equal(actual, expected)
