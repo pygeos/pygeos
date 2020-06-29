pygeos/pygeos

commit 6b77583a4cc0fb6c8f0d5983b45126d3afd06f6c

Implement wrappers for Fréchet distance under measurement module (3.7.0+) (#144)

src/ufuncs.c
@@ -530,6 +530,17 @@ static PyUFuncGenericFunction Y_i_funcs[1] = {&Y_i_func};
 /* Define the geom, geom -> double functions (YY_d) */
 static void *distance_data[1] = {GEOSDistance_r};
 static void *hausdorff_distance_data[1] = {GEOSHausdorffDistance_r};
+#if GEOS_SINCE_370
+  static int GEOSFrechetDistanceWrapped_r(void *context, void *a,  void *b, double *c) {
+      /* Handle empty geometries (they give segfaults) */
+      if (GEOSisEmpty_r(context, a) | GEOSisEmpty_r(context, b)) {
+          *c = NPY_NAN;
+          return 1;
+      }
+      return GEOSFrechetDistance_r(context, a, b, c);
+  }
+  static void *frechet_distance_data[1] = {GEOSFrechetDistanceWrapped_r};
+#endif
 /* Project and ProjectNormalize don't return error codes. wrap them. */
 static int GEOSProjectWrapped_r(void *context, void *a,  void *b, double *c) {
     /* Handle empty points (they give segfaults) */
@@ -720,6 +731,32 @@ static void haussdorf_distance_densify_func(char **args, npy_intp *dimensions,
     }
 }
 static PyUFuncGenericFunction haussdorf_distance_densify_funcs[1] = {&haussdorf_distance_densify_func};
+
+
+static char frechet_distance_densify_dtypes[4] = {NPY_OBJECT, NPY_OBJECT, NPY_DOUBLE, NPY_DOUBLE};
+static void frechet_distance_densify_func(char **args, npy_intp *dimensions,
+                                            npy_intp *steps, void *data)
+{
+    void *context_handle = geos_context[0];
+    GEOSGeometry *in1, *in2;
+
+    TERNARY_LOOP {
+        /* get the geometries: return on error */
+        if (!get_geom(*(GeometryObject **)ip1, &in1)) { return; }
+        if (!get_geom(*(GeometryObject **)ip2, &in2)) { return; }
+        double in3 = *(double *) ip3;
+        if ((in1 == NULL) | (in2 == NULL) | npy_isnan(in3) | GEOSisEmpty_r(context_handle, in1) | GEOSisEmpty_r(context_handle, in2)) {
+            *(double *)op1 = NPY_NAN;
+        } else {
+            if ((in3 <= 0) | (in3 > 1)) {
+                PyErr_Format(PyExc_ValueError, "Densify must be in range (0.0 - 1.0], got %f instead", in3);
+                return;
+            }
+            GEOSFrechetDistanceDensify_r(context_handle, in1, in2, in3, (double *) op1);
+        }
+    }
+}
+static PyUFuncGenericFunction frechet_distance_densify_funcs[1] = {&frechet_distance_densify_func};
 
 
 static char delaunay_triangles_dtypes[4] = {NPY_OBJECT, NPY_DOUBLE, NPY_BOOL, NPY_OBJECT};
@@ -1485,6 +1522,12 @@ int init_ufuncs(PyObject *m, PyObject *d)
     DEFINE_CUSTOM (to_wkb, 5);
     DEFINE_CUSTOM (to_wkt, 5);
     DEFINE_CUSTOM (from_shapely, 1);
+
+    #if GEOS_SINCE_370
+      DEFINE_YY_d (frechet_distance);
+
+      DEFINE_CUSTOM (frechet_distance_densify, 3);
+    #endif
 
     #if GEOS_SINCE_380
       DEFINE_Y_Y (make_valid);

pygeos/measurement.py
@@ -2,8 +2,10 @@ import numpy as np
 
 from . import lib
 from . import Geometry  # NOQA
+from .geos import requires_geos
 
-__all__ = ["area", "distance", "bounds", "total_bounds", "length", "hausdorff_distance"]
+
+__all__ = ["area", "distance", "bounds", "total_bounds", "length", "hausdorff_distance", "frechet_distance"]
 
 
 def area(geometry, **kwargs):
@@ -172,3 +174,41 @@ def hausdorff_distance(a, b, densify=None, **kwargs):
         return lib.hausdorff_distance(a, b, **kwargs)
     else:
         return lib.haussdorf_distance_densify(a, b, densify, **kwargs)
+
+
+@requires_geos("3.7.0")
+def frechet_distance(a, b, densify=None, **kwargs):
+    """Compute the discrete Fréchet distance between two geometries.
+
+    The Fréchet distance is a measure of similarity: it is the greatest
+    distance between any point in A and the closest point in B. The discrete
+    distance is an approximation of this metric: only vertices are considered.
+    The parameter 'densify' makes this approximation less coarse by splitting
+    the line segments between vertices before computing the distance.
+
+    Fréchet distance sweep continuously along their respective curves
+    and the direction of curves is significant. This makes it a better measure
+    of similarity than Hausdorff distance for curve or surface matching.
+
+    Parameters
+    ----------
+    a, b : Geometry or array_like
+    densify : float, array_like or None
+        The value of densify is required to be between 0 and 1.
+
+    Examples
+    --------
+    >>> line_1 = Geometry("LINESTRING (0 0, 100 0)")
+    >>> line_2 = Geometry("LINESTRING (0 0, 50 50, 100 0)")
+    >>> frechet_distance(line_1, line_2)  # doctest: +ELLIPSIS
+    70.71...
+    >>> frechet_distance(line_1, line_2, densify=0.5)
+    50.0
+    >>> frechet_distance(line_1, Geometry("LINESTRING EMPTY"))
+    nan
+    >>> frechet_distance(line_1, None)
+    nan
+    """
+    if densify is None:
+        return lib.frechet_distance(a, b, **kwargs)
+    return lib.frechet_distance_densify(a, b, densify, **kwargs)

pygeos/test/test_measurement.py
@@ -175,3 +175,117 @@ def test_haussdorf_distance_empty():
 def test_haussdorf_distance_densify_empty():
     actual = pygeos.hausdorff_distance(point, empty, densify=0.2)
     assert np.isnan(actual)
+
+
+@pytest.mark.skipif(pygeos.geos_version < (3, 7, 0), reason="GEOS < 3.7")
+@pytest.mark.parametrize(
+    "geom1, geom2, expected",
+    [
+        # identical geometries should have 0 distance
+        (
+            pygeos.linestrings([[0, 0], [100, 0]]),
+            pygeos.linestrings([[0, 0], [100, 0]]),
+            0,
+        ),
+        # example from GEOS docs
+        (
+            pygeos.linestrings([[0, 0], [50, 200], [100, 0], [150, 200], [200, 0]]),
+            pygeos.linestrings([[0, 200], [200, 150], [0, 100], [200, 50], [0, 0]]),
+            200
+        ),
+        # same geometries but different curve direction results in maximum
+        # distance between vertices on the lines.
+        (
+            pygeos.linestrings([[0, 0], [50, 200], [100, 0], [150, 200], [200, 0]]),
+            pygeos.linestrings([[200, 0], [150, 200], [100, 0], [50, 200], [0, 0]]),
+            200,
+        ),
+        # another example from GEOS docs
+        (
+            pygeos.linestrings([[0, 0], [50, 200], [100, 0], [150, 200], [200, 0]]),
+            pygeos.linestrings([[0, 0], [200, 50], [0, 100], [200, 150], [0, 200]]),
+            282.842712474619
+        ),
+        # example from GEOS tests
+        (
+            pygeos.linestrings([[0, 0], [100, 0]]),
+            pygeos.linestrings([[0, 0], [50, 50], [100, 0]]),
+            70.7106781186548
+        ),
+    ],
+)
+def test_frechet_distance(geom1, geom2, expected):
+    actual = pygeos.frechet_distance(geom1, geom2)
+    assert actual == pytest.approx(expected, abs=1e-12)
+
+
+@pytest.mark.skipif(pygeos.geos_version < (3, 7, 0), reason="GEOS < 3.7")
+@pytest.mark.parametrize(
+    "geom1, geom2, densify, expected",
+    [
+        # example from GEOS tests
+        (
+            pygeos.linestrings([[0, 0], [100, 0]]),
+            pygeos.linestrings([[0, 0], [50, 50], [100, 0]]),
+            0.001,
+            50
+        ),
+    ],
+)
+def test_frechet_distance_densify(geom1, geom2, densify, expected):
+    actual = pygeos.frechet_distance(geom1, geom2, densify=densify)
+    assert actual == pytest.approx(expected, abs=1e-12)
+
+
+@pytest.mark.skipif(pygeos.geos_version < (3, 7, 0), reason="GEOS < 3.7")
+@pytest.mark.parametrize(
+    "geom1, geom2",
+    [
+        (line_string, None),
+        (None, line_string),
+        (None, None),
+        (line_string, empty),
+        (empty, line_string),
+        (empty, empty),
+    ],
+)
+def test_frechet_distance_nan_for_invalid_geometry_inputs(geom1, geom2):
+    actual = pygeos.frechet_distance(geom1, geom2)
+    assert np.isnan(actual)
+
+
+@pytest.mark.skipif(pygeos.geos_version < (3, 7, 0), reason="GEOS < 3.7")
+def test_frechet_densify_ndarray():
+    actual = pygeos.frechet_distance(
+        pygeos.linestrings([[0, 0], [100, 0]]),
+        pygeos.linestrings([[0, 0], [50, 50], [100, 0]]),
+        densify=[0.1, 0.2, 1]
+    )
+    expected = np.array([50, 50.99019514, 70.7106781186548])
+    np.testing.assert_array_almost_equal(actual, expected)
+
+
+@pytest.mark.skipif(pygeos.geos_version < (3, 7, 0), reason="GEOS < 3.7")
+def test_frechet_densify_nan():
+    actual = pygeos.frechet_distance(line_string, line_string, densify=np.nan)
+    assert np.isnan(actual)
+
+
+@pytest.mark.skipif(pygeos.geos_version < (3, 7, 0), reason="GEOS < 3.7")
+@pytest.mark.parametrize(
+    "densify",
+    [
+        0,
+        -1,
+        2,
+    ]
+)
+def test_frechet_densify_invalid_values(densify):
+    with pytest.raises(ValueError, match=r"Densify must be in range \(0.0 - 1.0], got .* instead"):
+        actual = pygeos.frechet_distance(line_string, line_string, densify=densify)
+
+
+@pytest.mark.skipif(pygeos.geos_version < (3, 7, 0), reason="GEOS < 3.7")
+def test_frechet_distance_densify_empty():
+    actual = pygeos.frechet_distance(line_string, empty, densify=0.2)
+    assert np.isnan(actual)

commit 4fec6d643ed620ff5a28a195cbbddabca1d715fe

Fix memory leak in from_shapely (introduced in #144)

src/ufuncs.c
@@ -1305,11 +1305,13 @@ static void from_shapely_func(char **args, npy_intp *dimensions,
             if (attr == NULL) {
                 /* Raise if __geom__ does not exist */
                 PyErr_Format(PyExc_TypeError, "Expected a shapely object or None, got %s", Py_TYPE(in1)->tp_name);
+                GEOS_FINISH;
                 return;
             } else if (!PyLong_Check(attr)) {
                 /* Raise if __geom__ is of incorrect type */
                 PyErr_Format(PyExc_TypeError, "Expected int for the __geom__ attribute, got %s", Py_TYPE(attr)->tp_name);
                 Py_XDECREF(attr);
+                GEOS_FINISH;
                 return;
             }
             /* Convert it to a GEOSGeometry pointer */