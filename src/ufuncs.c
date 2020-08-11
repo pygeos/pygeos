f85124284a43e350924dee2352373484d79b786e
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-08-11 20:22:24 +0200

    ENH: Add get_z ufunc

--- src/ufuncs.c
+++ src/ufuncs.c
@@ -699,6 +699,19 @@ static int GetY(void *context, void *a, double *b) {
     }
 }
 static void *get_y_data[1] = {GetY};
+static int GetZ(void *context, void *a, double *b) {
+    char typ = GEOSGeomTypeId_r(context, a);
+    if (typ != 0) {
+        *(double *)b = NPY_NAN;
+        return 1;
+    // } else if (GEOSGeom_getCoordinateDimension_r(context, a) != 3) {
+    //     *(double *)b = NPY_NAN;
+    //     return 0;
+    } else {
+        return GEOSGeomGetZ_r(context, a, b);
+    }
+}
+static void *get_z_data[1] = {GetZ};
 static void *area_data[1] = {GEOSArea_r};
 static void *length_data[1] = {GEOSLength_r};
 typedef int FuncGEOS_Y_d(void *context, void *a, double *b);
@@ -1896,6 +1909,7 @@ int init_ufuncs(PyObject *m, PyObject *d)
 
     DEFINE_Y_d (get_x);
     DEFINE_Y_d (get_y);
+    DEFINE_Y_d (get_z);
     DEFINE_Y_d (area);
     DEFINE_Y_d (length);
 
