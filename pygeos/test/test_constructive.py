9aadf25a0e7e3e1863fdc6989dead4d5b0da6758
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-11-05 18:12:07 +0100

    ENH: offset_curve

--- pygeos/test/test_constructive.py
+++ pygeos/test/test_constructive.py
@@ -18,6 +18,7 @@ CONSTRUCTIVE_NO_ARGS = (
 
 CONSTRUCTIVE_FLOAT_ARG = (
     pygeos.buffer,
+    pygeos.offset_curve,
     pygeos.delaunay_triangles,
     pygeos.simplify,
     pygeos.voronoi_polygons,
@@ -35,6 +36,10 @@ def test_no_args_array(geometry, func):
 @pytest.mark.parametrize("geometry", all_types)
 @pytest.mark.parametrize("func", CONSTRUCTIVE_FLOAT_ARG)
 def test_float_arg_array(geometry, func):
+    if func is pygeos.offset_curve and pygeos.get_type_id(geometry) not in [1, 2]:
+        with pytest.raises(GEOSException):
+            func([geometry, geometry], 0.0)
+        return
     actual = func([geometry, geometry], 0.0)
     assert actual.shape == (2,)
     assert isinstance(actual[0], Geometry)
@@ -63,6 +68,10 @@ def test_float_arg_missing(func):
 @pytest.mark.parametrize("geometry", all_types)
 @pytest.mark.parametrize("func", CONSTRUCTIVE_FLOAT_ARG)
 def test_float_arg_nan(geometry, func):
+    if func is pygeos.offset_curve and pygeos.get_type_id(geometry) not in [1, 2]:
+        with pytest.raises(GEOSException):
+            func(geometry, float("nan"))
+        return
     actual = func(geometry, float("nan"))
     assert actual is None
 
