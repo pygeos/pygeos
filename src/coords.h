828408111bf712de4e4ccdaf30923a40bc8b8a06
Brendan C. Ward <bcward@astutespruce.com>
2021-02-24 14:49:08 -0800

    Fix bounds and box C header entries

--- src/coords.h
+++ src/coords.h
@@ -5,10 +5,6 @@
 
 #include "geos.h"
 
-int get_bounds(GEOSContextHandle_t ctx, GEOSGeometry* geom, double* xmin, double* ymin,
-               double* xmax, double* ymax);
-GEOSGeometry* create_box(GEOSContextHandle_t ctx, double xmin, double ymin, double xmax,
-                         double ymax);
 
 extern PyObject* PyCountCoords(PyObject* self, PyObject* args);
 extern PyObject* PyGetCoords(PyObject* self, PyObject* args);
