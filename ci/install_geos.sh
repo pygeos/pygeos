ec3a484840e85298343933bffaaff06bce2f0460
caspervdw <caspervdw@gmail.com>
2021-08-26 13:38:50 +0200

    Fix script and prune cache

--- ci/install_geos.sh
+++ ci/install_geos.sh
@@ -38,7 +38,7 @@ build_geos(){
     mkdir build
     cd build
     cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$GEOS_INSTALL ..
-    cmake --build -j 2 .
+    cmake --build . -j 2
     # ctest .
     cmake --install .
 }
