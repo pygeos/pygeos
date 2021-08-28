8cd9a34745f916287c9164140b56b4ebcbb1674d
caspervdw <caspervdw@gmail.com>
2021-08-28 14:36:06 +0200

    Fix

--- ci/install_geos.sh
+++ ci/install_geos.sh
@@ -17,7 +17,7 @@ elif [ -z "$GEOS_VERSION" ]; then
 fi
 
 # Create directories, if they don't exist
-mkdir -p "$GEOS_INSTALL"
+mkdir -p $GEOS_INSTALL
 
 # Download and build GEOS outside other source tree
 if [ -z "$GEOS_BUILD" ]; then
@@ -37,8 +37,7 @@ build_geos(){
     echo "Building geos-$GEOS_VERSION"
     mkdir build
     cd build
-    GEOS_INSTALL_FW=$(printf "%s" "$GEOS_INSTALL" | sed 's/\\/\//g')
-    cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$GEOS_INSTALL_FW ..
+    cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$GEOS_INSTALL ..
     cmake --build . -j 2
     # ctest .
     cmake --install .
