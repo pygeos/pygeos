cedc74637c7ba237861039059604b185bf755ffd
caspervdw <caspervdw@gmail.com>
2021-10-22 14:17:46 +0200

    Skip pre-3.10

--- pygeos/tests/test_geometry.py
+++ pygeos/tests/test_geometry.py
@@ -511,7 +511,9 @@ def test_set_precision_grid_size_nan():
     assert pygeos.set_precision(pygeos.Geometry("POINT (0.9 0.9)"), np.nan) is None
 
 
-@pytest.mark.skipif(pygeos.geos_version < (3, 6, 0), reason="GEOS < 3.6")
+@pytest.mark.skipif(
+    pygeos.geos_version < (3, 10, 0), reason="Tests are only for GEOS 3.10"
+)
 @pytest.mark.parametrize(
     "geometry,flags,expected",
     [
@@ -539,13 +541,10 @@ def test_set_precision_grid_size_nan():
             "make_valid",
             pygeos.Geometry("LINESTRING EMPTY"),
         ),
-        pytest.param(
+        (
             pygeos.Geometry("LINESTRING (0 0, 0.1 0.1)"),
             "no_topo",
             pygeos.Geometry("LINESTRING (0 0, 0 0)"),
-            marks=pytest.mark.skipif(
-                pygeos.geos_version < (3, 10, 0), reason="Pre GEOS 3.10.0"
-            ),
         ),
         (
             pygeos.Geometry("LINESTRING (0 0, 0.1 0.1)"),
@@ -560,13 +559,10 @@ def test_set_precision_grid_size_nan():
                 pygeos.geos_version == (3, 10, 0), reason="Segfaults on GEOS 3.10.0"
             ),
         ),
-        pytest.param(
+        (
             pygeos.Geometry("LINEARRING (0 0, 0.1 0, 0.1 0.1, 0 0.1, 0 0)"),
             "no_topo",
             pygeos.Geometry("LINEARRING (0 0, 0 0, 0 0, 0 0, 0 0)"),
-            marks=pytest.mark.skipif(
-                pygeos.geos_version < (3, 10, 0), reason="Pre GEOS 3.10.0"
-            ),
         ),
         pytest.param(
             pygeos.Geometry("LINEARRING (0 0, 0.1 0, 0.1 0.1, 0 0.1, 0 0)"),
@@ -581,13 +577,10 @@ def test_set_precision_grid_size_nan():
             "make_valid",
             pygeos.Geometry("POLYGON EMPTY"),
         ),
-        pytest.param(
+        (
             pygeos.Geometry("POLYGON ((0 0, 0.1 0, 0.1 0.1, 0 0.1, 0 0))"),
             "no_topo",
             pygeos.Geometry("POLYGON ((0 0, 0 0, 0 0, 0 0, 0 0))"),
-            marks=pytest.mark.skipif(
-                pygeos.geos_version < (3, 10, 0), reason="GEOS 3.10.0"
-            ),
         ),
         (
             pygeos.Geometry("POLYGON ((0 0, 0.1 0, 0.1 0.1, 0 0.1, 0 0))"),
