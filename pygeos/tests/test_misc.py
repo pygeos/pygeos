88bb5f524d7f71f7af2026189ddf596419970996
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2021-10-31 14:02:11 +0100

    Refactor test

--- pygeos/tests/test_misc.py
+++ pygeos/tests/test_misc.py
@@ -189,45 +189,39 @@ def test_multithreading_enabled_ok(args, kwargs):
     assert result[0] == 42
 
 
-def test_may_segfault():
-    @may_segfault
-    def my_func():
+@may_segfault
+def may_unstable_func(event=None):
+    if event == "segfault":
         ctypes.string_at(0)  # segfault
+    elif event == "exit":
+        exit(1)
+    elif event == "raise":
+        raise ValueError("This is a test")
+    elif event == "warn":
+        warnings.warn("This is a test", RuntimeWarning)
+    elif event == "return":
+        return "This is a test"
 
+
+def test_may_segfault():
     with pytest.raises(GEOSException, match="GEOS crashed with exit code"):
-        my_func()
+        may_unstable_func("segfault")
 
 
 def test_may_segfault_exit():
-    @may_segfault
-    def my_func():
-        exit(1)
-
     with pytest.raises(GEOSException, match="GEOS crashed with exit code 1."):
-        my_func()
+        may_unstable_func("exit")
 
 
 def test_may_segfault_raises():
-    @may_segfault
-    def my_func():
-        raise GEOSException("This is a test")
-
-    with pytest.raises(GEOSException, match="This is a test"):
-        my_func()
+    with pytest.raises(ValueError, match="This is a test"):
+        may_unstable_func("raise")
 
 
 def test_may_segfault_returns():
-    @may_segfault
-    def my_func():
-        return "This is a test"
-
-    assert my_func() == "This is a test"
+    assert may_unstable_func("return") == "This is a test"
 
 
 def test_may_segfault_warns():
-    @may_segfault
-    def my_func():
-        warnings.warn("This is a test", UserWarning)
-
-    with pytest.warns(UserWarning, match="This is a test"):
-        my_func()
+    with pytest.warns(RuntimeWarning, match="This is a test"):
+        may_unstable_func("warn")
